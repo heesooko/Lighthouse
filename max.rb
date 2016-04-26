4d5307df0b0341fa3322d58bcb2d0f9428f38b0e
heesooko <heesooko@sas.upenn.edu>
2016-04-26 12:30:05 -0400

    Create max.rb

---
+++ max.rb
@@ -0,0 +1,25 @@
+# Find the maximum 
+def maximum(arr)
+number = arr[0]
+
+arr.each do |i|
+ if i > number
+   number = i
+ end
+end
+number
+end
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"

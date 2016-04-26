d45dbaaedf802b75eec9c9e17e9af96935463984
heesooko <heesooko@sas.upenn.edu>
2016-04-26 17:43:38 -0400

    Create bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,55 @@
+def sort(arr)
+  arr.sort
+end
+
+def bubble_sort(arr)
+  n = arr.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  arr
+end
+
+# Find the maximum 
+def maximum(arr)
+  bubble_sort(arr).last
+end
+
+def minimum(arr)
+  bubble_sort(arr).first
+end
+
+# testing bubble sort
+result = bubble_sort([4,2,9,7])
+puts "#{result}"
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return 2 below
+result = minimum([2, 42, 22, 02])
+puts "min of 2, 42, 22, 02 is: #{result}"
+
+ 
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+result = minimum([])
+puts "min on empty set is: #{result.inspect}"
+ 
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+ 
+result = maximum([6])
+puts "max of just 6 is: #{result}"
